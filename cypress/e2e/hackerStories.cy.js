50fd78eddd9ed72db3d28e4886ef1f39b7fa276c
KarlaMiriam <kmds@gft.com>
2023-06-12 14:18:02 -0300

    update hackerStories

--- cypress/e2e/hackerStories.cy.js
+++ cypress/e2e/hackerStories.cy.js
@@ -275,7 +275,7 @@ describe('Hacker Stories', () => {
       })
 
       context('Last searches', () => {
-        it.only('shows a max of 5 buttons for the last searched terms', () => {
+        it('shows a max of 5 buttons for the last searched terms', () => {
           const faker = require('faker')
 
           cy.intercept(
